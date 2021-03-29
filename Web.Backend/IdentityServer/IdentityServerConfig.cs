e162d31ca556969a76c520f30a45c8408b9604d1
vantrieu <nguyenvantrieu.it@gmail.com>
2021-03-29 15:34:36 +0700

    Backend - update client setting and correct spelling

--- Web.Backend/IdentityServer/IdentityServerConfig.cs
+++ Web.Backend/IdentityServer/IdentityServerConfig.cs
@@ -16,7 +16,7 @@ namespace Web.Backend.IdentityServer
         public static IEnumerable<ApiScope> ApiScopes =>
              new ApiScope[]
              {
-                  new ApiScope("rokieshop.api", "Rookie Shop API")
+                  new ApiScope("rookieshop.api", "Rookie Shop API")
              };
 
         public static IEnumerable<Client> Clients =>
@@ -41,9 +41,9 @@ namespace Web.Backend.IdentityServer
 
                     AllowedGrantTypes = GrantTypes.Code,
 
-                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+                    RedirectUris = { "https://localhost:44325/signin-oidc" },
 
-                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:44325/signout-callback-oidc" },
 
                     AllowedScopes = new List<string>
                     {
