a29e935ed1b344c2aa2c2a768d76c136aaddbb00
vantrieu <nguyenvantrieu.it@gmail.com>
2021-03-30 11:32:26 +0700

    seed data when first run app

---
+++ Web.Backend/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Backend.Models;
+
+namespace Web.Backend.Data
+{
+    public class ApplicationDbContextSeed
+    {
+        public static async Task SeedEssentialsAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            if (!roleManager.RoleExistsAsync("superadmin").Result)
+            {
+                await roleManager.CreateAsync(new IdentityRole("superadmin"));
+            }
+            if (!roleManager.RoleExistsAsync("admin").Result)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (!roleManager.RoleExistsAsync("user").Result)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            //Seed Default User
+            var defaultUser = new User
+            {
+                UserName = "superadmin@gmail.com",
+                Email = "superadmin@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "0867537750",
+                FullName = "Super Admin",
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.Count(u => u.Email == defaultUser.Email) == 0)
+            {
+                IdentityResult result = await userManager.CreateAsync(defaultUser, "Qpzm1092@");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(defaultUser, "superadmin");
+                    await userManager.AddToRoleAsync(defaultUser, "admin");
+                    await userManager.AddToRoleAsync(defaultUser, "user");
+                }
+            }
+        }
+    }
+}
--- Web.Backend/Program.cs
+++ Web.Backend/Program.cs
@@ -1,19 +1,47 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web.Backend.Data;
+using Web.Backend.Models;
 
 namespace Web.Backend
 {
     public class Program
     {
-        public static void Main(string[] args)
+        //public static void Main(string[] args)
+        //{
+        //    CreateHostBuilder(args).Build().Run();
+        //}
+
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args)
+                         .Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    //Seed Default Users
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await ApplicationDbContextSeed.SeedEssentialsAsync(userManager, roleManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
