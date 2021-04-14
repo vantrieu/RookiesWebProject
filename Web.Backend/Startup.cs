1a75b2a5098bd9a8e5730f406bf52e8d85d004b4
vantrieu <nguyenvantrieu.it@gmail.com>
2021-04-14 16:05:27 +0700

    config CORS

--- Web.Backend/Startup.cs
+++ Web.Backend/Startup.cs
@@ -31,6 +31,14 @@ namespace Web.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin", builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -168,6 +176,7 @@ namespace Web.Backend
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseCors("AllowAnyOrigin");
 
             //app.UseAuthentication();
             app.UseIdentityServer();
